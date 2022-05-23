03d41ae593973eed506d7d566a87cd0b2d13c86b
fares <fares.steen@gmail.com>
2022-05-23 20:57:13 +0200

    add tests

--- IntegrationTests/IntegrationTests/ProductControllerTest.cs
+++ IntegrationTests/IntegrationTests/ProductControllerTest.cs
@@ -8,20 +8,26 @@ namespace IntegrationTests;
 [TestClass]
 public class ProductControllerTest
 {
-    private readonly SetUpTestEnvironment setUpTestEnvironment;
+    private readonly SetUpTestEnvironment2 setUpTestEnvironment2;
+    private readonly SetUpTestEnvironment1 setUpTestEnvironment1;
 
     public ProductControllerTest()
     {
-        setUpTestEnvironment = new SetUpTestEnvironment();
+        setUpTestEnvironment2 = new SetUpTestEnvironment2();
+        setUpTestEnvironment1 = new SetUpTestEnvironment1(setUpTestEnvironment2.testClient);
     }
 
     [TestMethod]
     public async Task TestMethod1()
     {
-        var response= await setUpTestEnvironment.TestClient.GetAsync($"/Product");
+        var response= await setUpTestEnvironment1.testClient.GetAsync($"/Product");
         var result = await response.Content.ReadAsStringAsync();
         
-        var productFromController =
-            JsonConvert.DeserializeObject<Product>(await response.Content.ReadAsStringAsync());
+        var fullProduct =
+            JsonConvert.DeserializeObject<FullProduct>(await response.Content.ReadAsStringAsync());
+        
+        Assert.AreEqual(fullProduct.Product.Name, "Product 1");
+        Assert.AreEqual(fullProduct.Product.Description, "im a product");
+        Assert.AreEqual(fullProduct.ProductDetails.Price, 40);
     }
 }
--- IntegrationTests/IntegrationTests/SetUpTestEnvironment.cs
+++ IntegrationTests/IntegrationTests/SetUpTestEnvironment.cs
@@ -1,14 +1,44 @@
 using System.Net.Http;
 using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Options;
+using Service1;
+
 
 namespace IntegrationTests;
 
-internal class SetUpTestEnvironment : WebApplicationFactory<Program>
+internal class SetUpTestEnvironment2 : WebApplicationFactory<Service2.Program>
 {
-    internal readonly HttpClient TestClient;
+    internal readonly HttpClient testClient;
 
-    public SetUpTestEnvironment()
+    public SetUpTestEnvironment2()
     {
-        TestClient = CreateClient();
+        testClient = CreateClient();
+    }
+}
+
+internal class SetUpTestEnvironment1 : WebApplicationFactory<Service1.Program>
+{
+    internal readonly HttpClient testClient;
+    private readonly HttpClient _service2Client;
+    public SetUpTestEnvironment1(HttpClient service2Client)
+    {
+        _service2Client = service2Client;
+        testClient = CreateClient();
+    }
+    
+    protected override IHost CreateHost(IHostBuilder builder)
+    {
+        builder.ConfigureServices(serviceCollection =>
+        {
+            serviceCollection.Configure<ServicesOption>(options =>
+            {
+                options.Service2ApiUrl = "";
+            });
+            serviceCollection.AddSingleton(_service2Client);
+        });
+
+        return base.CreateHost(builder);
     }
 }
